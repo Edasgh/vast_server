8ca577c1e4e216e56b590cc55924c2eb7f27dee8
Edasgh <edas25564@gmail.com>
2026-03-26 23:06:24 +0530

    Add client base url in socket server

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // React app's URL
+    origin: process.env.CLIENT_BASE_URL || "http://localhost:5173", // React app's URL
     methods: ["GET", "POST"],
     credentials: true
   },
