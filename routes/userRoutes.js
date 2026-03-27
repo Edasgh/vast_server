8767513a45550f79229d91f3f572e9b99cb7e58e
Edasgh <edas25564@gmail.com>
2026-03-27 09:36:34 +0530

    "fix logout controller"

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -75,12 +75,13 @@ const loginUser = async_handler(async (req, res) => {
 
 //logout user
 const logoutUser = (req, res) => {
-    res.cookie("token", "", {
+    res.clearCookie("token", {
         httpOnly: true,
-        expires: new Date(0),
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        path: "/",
     });
-
-    res.status(200).json({ message: "Logged out" });
+    res.status(200).json({ message: "Logged out successfully" });
 };
 
 //function to view logged in user's details
