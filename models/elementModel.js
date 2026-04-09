2a2642f487a0a41a719520ff30c3c9c41cd2ec78
Edasgh <edas25564@gmail.com>
2026-04-09 14:01:25 +0530

    "element schema -> arrow shape added"

--- models/elementModel.js
+++ models/elementModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const ElementSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
-    type: { type: String, enum: ['path', 'image', 'rect', 'circle', 'triangle'], required: true },
+    type: { type: String, enum: ['path', 'image', 'rect', 'circle', 'triangle','arrow'], required: true },
     mode: { type: String, enum: ['brush', 'eraser', 'shape'] },
     color: { type: String },
     // For paths: an array of coordinate objects
